5c65fa006232066a2d67df47d3f7793a19c682e7
Atharv Gulati <atharvgulati@Atharvs-MacBook-Pro.local>
2023-07-26 19:01:16 +0530

    new

---
+++ src/components/HomePage/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import navImage from "../../assets/12th Project/Logo Files/PNGs & SVGs/1x/Asset 3.png";
+import "../../styles/navbar.css";
+const Navbar = () => {
+  return (
+    <div className='navbar-full'>
+      <nav className='navbar'>
+        <div className='img-nav'>
+          <img src={navImage} alt='' />
+        </div>
+        <ul className='nav-items'>
+          <li className='item'>Our Projects</li>
+          <li className='item'>Our Approach</li>
+          <li className='item'>Contact Us</li>
+          <li className='btn-item item'>
+            <button className='btn nav-btn'>Get Started</button>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,27 +1,28 @@
-import React from 'react'
+import React from "react";
 import Section1 from "../components/HomePage/Section1";
 import Section2 from "../components/HomePage/Section2";
 import Section3 from "../components/HomePage/Section3";
 import Section4 from "../components/HomePage/Section4";
-import Section5 from '../components/HomePage/Section5'
-import Section6 from '../components/HomePage/Section6'
-import Section7 from '../components/HomePage/Section7'
-import Section8 from '../components/HomePage/Section8'
+import Section5 from "../components/HomePage/Section5";
+import Section6 from "../components/HomePage/Section6";
+import Section7 from "../components/HomePage/Section7";
+import Section8 from "../components/HomePage/Section8";
 import Section25 from "../components/HomePage/Section25";
-
+import Navbar from "../components/HomePage/Navbar";
 
 const HomePage = () => {
   return (
     <div>
-        <Section1 />
-        <Section2 />
-        <Section3 />
-        <Section4 />
-        <Section5 />
-        <Section6 />
-        <Section7 />
-        <Section8 />
-        <Section25 />
+      <Navbar />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+      <Section6 />
+      <Section7 />
+      <Section8 />
+      <Section25 />
     </div>
   );
 };
