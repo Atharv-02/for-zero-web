79b9b8240ef3a6774e2bea07957de50643a7c9e4
Atharv Gulati <atharvgulati@Atharvs-MacBook-Pro.local>
2023-07-28 14:36:41 +0530

    new

--- src/pages/OurApproach.jsx
+++ src/pages/OurApproach.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Section1 from "../components/OurApproach/Section1";
 import "../styles/ourApproach.css";
+import Footer from "../components/common/Footer";
+
+import Section6 from "../components/HomePage/Section6";
 
 const OurApproach = () => {
   return (
     <div>
       <Section1 />
+      <Section6 />
+      <Footer />
     </div>
   );
 };
