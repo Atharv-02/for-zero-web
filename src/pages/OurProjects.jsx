cbc4f022033bccf5c752a1357d8841312a833746
Navansh <khandelwalnavansh1@gmail.com>
2023-07-29 18:27:03 +0530

    do

--- src/pages/OurProjects.jsx
+++ src/pages/OurProjects.jsx
@@ -1,14 +1,17 @@
-import React from "react";
-import Navbar from "../components/HomePage/Navbar";
-import Section1 from "../components/OurProjects/Section1";
+import React from 'react'
+import Navbar from '../components/HomePage/Navbar'
+import Section1 from '../components/OurProjects/Section1'
+import Section4 from '../components/OurProjects/Section4'
 import Section3 from "../components/OurProjects/Section3";
 import "../styles/ourProjects.css";
+
 const OurProjects = () => {
   return (
     <div>
-      <Navbar />
-      <Section1 />
-      <Section3 />
+        <Navbar />
+        <Section1 />
+        <Section3 />
+        <Section4 />
     </div>
   );
 };
